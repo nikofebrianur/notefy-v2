97b4144bf342abab88af22a93b2dda3d1a4fedae
Niko Febrianur <nikofebrianur@gmail.com>
2022-12-03 22:37:10 +0800

    add routes for edit and new note

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import NotFoundMessage from "./pages/not-found";
 import ArchivesPage from './pages/archives'
 import NavMenu from "./components/layout/NavMenu";
 import IdNoteMainPage from './pages/notes/_id-main'
+import NewNotePage from "./pages/notes/new";
+import IdEditNotePage from "./pages/notes/_id-edit";
 
 function App() {
   return (
@@ -21,7 +23,9 @@ function App() {
           <Route path="/" element={<IndexPage />} />
           <Route path="/archives" element={<ArchivesPage />} />
           <Route path="/notes" element={<Navigate to="/" replace />} />
-          <Route path="/notes/:id" element={<IdNoteMainPage />} />
+          <Route path="/notes/new" element={<NewNotePage />} />
+		  <Route path="/notes/:id" element={<IdNoteMainPage />} />
+		  <Route path="/notes/:id/edit" element={<IdEditNotePage />} />
 		  <Route path="*" element={<NotFoundMessage />} />
         </Routes>
       </main>
