8dac56950fc854e7d89c1b592be2511277eee166
Niko Febrianur <nikofebrianur@gmail.com>
2022-12-03 22:36:41 +0800

    add new note page

---
+++ src/pages/notes/new.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function NewNotePage() {
+	const navigate = useNavigate();
+
+	return (
+		<div>
+			new note page
+		</div>
+	)
+}
