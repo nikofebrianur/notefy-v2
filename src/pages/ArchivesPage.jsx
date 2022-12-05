16bf7da27c9560812c67a3ff812617ffb1e2cad0
Niko Febrianur <nikofebrianur@gmail.com>
2022-12-05 19:43:41 +0800

    fix search feature in archived notes

--- src/pages/ArchivesPage.jsx
+++ src/pages/ArchivesPage.jsx
@@ -1,21 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import Home from '../components/index/Home';
-import NoteList from '../components/notes/NoteList';
-import NoteListEmpty from '../components/notes/NoteListEmpty';
-import { getArchivedNotes } from '../utils/local-data';
+import React, { useEffect, useState } from "react";
+import Home from "../components/index/Home";
+import NoteList from "../components/notes/NoteList";
+import NoteListEmpty from "../components/notes/NoteListEmpty";
+import { getArchivedNotes } from "../utils/local-data";
 
 export default function ArchivesPage() {
-	const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [search, setSearch] = useState("");
 
-	useEffect(() => {
-		setNotes(getArchivedNotes())
-	}, [])
+  const onSearchHandler = (event) => {
+    setSearch(event.target.value);
+  };
 
-	return (
-    <div className='homepage'>
+  useEffect(() => {
+    if (search !== "") {
+      setNotes(
+        getArchivedNotes().filter((note) =>
+          note.title.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    } else {
+      setNotes(getArchivedNotes());
+    }
+  }, [search]);
+
+  return (
+    <div className="homepage">
       <h2>Archived Notes</h2>
-      <div className='search-bar'>
-        <input type='text' placeholder='Search archived notes by title...' />
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search archived notes by title..."
+          value={search}
+          onChange={onSearchHandler}
+        />
       </div>
       {notes.length > 0 && <NoteList notes={notes} />}
       {notes.length === 0 && <NoteListEmpty />}
